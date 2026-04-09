3efe6b7ff1c15e48af87692ce5ce7f2fb7d389bb
Ramanand <ramuttenravitsr2002@gmail.com>
2026-04-09 19:16:42 +0530

    fix: route all traffic through Fastify serverless handler

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,11 @@
 import { buildApp } from '../src/app';
 
+let app: any;
+
 export default async (req: any, res: any) => {
-  const app = await buildApp();
+  if (!app) {
+    app = await buildApp();
+  }
   await app.ready();
   app.server.emit('request', req, res);
 };
